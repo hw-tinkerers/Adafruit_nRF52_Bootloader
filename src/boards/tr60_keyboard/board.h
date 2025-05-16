a57a1186325429403a62afbac0bf058da4ab7e31
Srishtik Bhandarkar <srishtik.bhandarkar2000@gmail.com>
2025-05-16 10:05:35 +0200

    Chenged licensing details

--- src/boards/tr60_keyboard/board.h
+++ src/boards/tr60_keyboard/board.h
@@ -1,7 +1,7 @@
 /*
  * The MIT License (MIT)
  *
- * Copyright (c) 2020 Nick Winans
+ * Copyright (c) 2025 Srishtik Bhandarkar
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
